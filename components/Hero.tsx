8fb488638de92c11cf666ee07acd771f3ca5be66
Roland Stedy <stedyroland@gmail.com>
2025-01-23 16:50:19 +0300

    button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ function Hero() {
       <div className="w-8/12 max-sm:w-11/12 mx-auto bg-black text-white text-center h-[70vh] flex flex-col p-10  rounded-xl justify-between">
         <p className="text-lg font_regular font-[24]">Hi, <br />I am Stedy And...</p>
         <h1 className="heading_font ">I <br />Make <br />stuff</h1>
-        <Button className="w-1/2 font_regular m-auto bg-black text-white border-2 border-white font-semibold text-lg hover:bg-white hover:text-black">
+        <Button className="w-1/2 px-2 font_regular m-auto bg-black text-white border-2 border-white font-semibold text-lg hover:bg-white hover:text-black">
           Explore more
         </Button>
       </div>
