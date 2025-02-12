d086d90e39ecc1708ab14a28a49e03f2d52b2f13
Roland Stedy <stedyroland@gmail.com>
2025-02-12 18:22:24 +0300

    nav sidebar update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,21 @@
+'use client'
+
 import { NavLinks } from '@/constants'
-import { MenuIcon } from 'lucide-react'
+import { MenuIcon, X } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 function NavBar() {
+  function showSidebar(){
+    const sidebar = document.getElementById('sidebar')!
+    sidebar.style.display = 'block'
+  }
+  function closeSideBar(){
+     const sidebar = document.getElementById('sidebar')!
+    sidebar.style.display = 'none'
+  }
+
   return (
     <nav className=''>
       <div className='flex justify-between p-4 '>
@@ -17,6 +28,25 @@ function NavBar() {
           />
         </Link>
         <div>
+        <ul id='sidebar' className='font_regular hidden fixed top-0 right-0 rounded-xl 
+          z-10 bg-white shadow-sm px-12   py-8'>
+            <button 
+              onClick={closeSideBar} 
+              className='flex w-10/12 hover:bg-black rounded-lg py-1 hover:text-white m-auto justify-center'
+            >
+              <X className='flex justify-center'/>
+            </button>
+            {NavLinks.map((link)=>(
+              <li
+                key={link.label}
+                className='border-2  border-black hover:bg-black hover:text-white rounded-lg px-4 py-1 my-2 text-center'
+              >
+                <Link href={link.route}>
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
           <ul className='flex gap-8 font_regular max-sm:hidden'>
             {NavLinks.map((link)=>(
               <li
@@ -38,7 +68,7 @@ function NavBar() {
             className='max-sm:hidden'
           />
         </Link>
-        <Link href='/' className='hidden max-sm:block'>
+        <Link href='/' onClick={showSidebar} className='hidden max-sm:block'>
           <MenuIcon />
         </Link>
       </div>
